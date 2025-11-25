434271a4cf965bf99c7f2874e85c3a6d2754f185
Saumya Verma <saumyaverma46@gmail.com>
2025-11-25 16:51:02 +0530

    Add ChildC component for theme toggling

---
+++ src/components/ChildC.jsx
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react'
+import { themeContext } from '../App'
+
+const ChildC = () => {
+
+  const [theme, setTheme] = useContext(themeContext);
+
+  function handleClick () {
+    if(theme==='light')
+      setTheme('dark')
+    else
+      setTheme('light')
+  }
+  return (
+    <div>
+       <button onClick={handleClick}>
+        click me
+       </button>
+    </div>
+  )
+}
+
+export default ChildC
